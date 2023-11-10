1ec92010ef27047e46f3987999adb6797ec08d89
Thinker92 <vilsonapo@gmail.com>
2023-11-09 20:16:14 -0500

    Added header with nav bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation'
+
+function Header(){
+    return (
+        <header>
+            <h1>Vilson Apostolovski II</h1>
+            <Navigation />
+        </header>
+    );
+}
+
+export default Header;
